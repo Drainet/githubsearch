27c578f7c13f665d3a310a9738d75c06a361fe79
drain <drain.cy.chang@gmail.com>
2019-10-15 12:21:49 +0800

    apply main view basic layout

--- githubsearch/MainViewController.swift
+++ githubsearch/MainViewController.swift
@@ -7,15 +7,40 @@
 //
 
 import UIKit
+import SnapKit
 
-class MainViewController: UIViewController {
-    
-    required init?(coder: NSCoder) {
+class MainViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    required init?(coder _: NSCoder) {
         fatalError("not supported")
     }
-    
+
     init() {
         super.init(nibName: nil, bundle: nil)
+    }
+
+    let textField = UITextField()
+
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+
+    override func loadView() {
+        super.loadView()
+        view.addSubview(textField)
+        view.addSubview(collectionView)
+
+        textField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(60)
+        }
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(textField.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
     }
 
     override func viewDidLoad() {
@@ -23,4 +48,15 @@ class MainViewController: UIViewController {
         view.backgroundColor = .blue
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
 }
